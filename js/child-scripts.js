d563ebb0fd78629b48922c4b84d5a58c55f6fb81
Kaleb Link <kaleb.s.link@gmail.com>
2025-06-26 13:56:37 -0700

    added widget styling to event plugin through child script

--- js/child-scripts.js
+++ js/child-scripts.js
@@ -1,1 +1,6 @@
 // console.log("Hello Child Theme World!");
+
+// add widget class to event widget
+let eventWidget = document.querySelector('#secondary .tribe-compatibility-container');
+
+eventWidget.classList.add('widget');
