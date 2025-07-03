ee185bd9cf304d5c643011bcc42169562e59890a
Kaleb Link <kaleb.s.link@gmail.com>
2025-07-03 11:03:00 -0700

    added script that removes target attribute

--- js/child-scripts.js
+++ js/child-scripts.js
@@ -1,6 +1,10 @@
 // console.log("Hello Child Theme World!");
 
-// add widget class to event widget
 let eventWidget = document.querySelector('#secondary .tribe-compatibility-container');
+let headerButton = document.querySelector('.header-custom-button');
 
+// add widget class to event widget
 eventWidget.classList.add('widget');
+
+// remove target blank attribute from header button so it opens in the same page
+headerButton.removeAttribute('target');
