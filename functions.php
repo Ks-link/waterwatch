8f265949f2fbb279ea497db3a18d93abfe50c626
Kaleb Link <kaleb.s.link@gmail.com>
2025-07-11 09:43:41 -0700

    added custom widget area, video widget explaining how to create posts

--- functions.php
+++ functions.php
@@ -30,3 +30,63 @@ add_action(
     'wp_enqueue_scripts',
     'waterwatch_enqueue_styles'
 );
+
+/**
+ * Lower Yoast SEO Metabox location
+ */
+function yoast_to_bottom()
+{
+    return 'low';
+}
+add_filter('wpseo_metabox_prio', 'yoast_to_bottom');
+
+
+function add_custom_dashboard_widget()
+{
+    wp_add_dashboard_widget(
+        'custom_help_widget', // Widget ID
+        'Here are some tutorials!',  // Widget title
+        'custom_dashboard_help' // Callback function
+    );
+}
+
+/**
+ * Remove unwanted dashboard widgets and add video tutorial widget
+ */
+function remove_unwanted_dashboard_widgets()
+{
+    // Remove "At a Glance" widget
+    // remove_meta_box('dashboard_right_now', 'dashboard', 'normal');
+
+    // Remove "Welcome" panel
+    remove_action('welcome_panel', 'wp_welcome_panel');
+
+    // Remove "WordPress Events and News" widget
+    remove_meta_box('dashboard_primary', 'dashboard', 'side');
+
+    // Remove "Activity" widget
+    remove_meta_box('dashboard_activity', 'dashboard', 'normal');
+
+    remove_meta_box("dashboard_quick_press", 'dashboard', 'side');
+    
+    // remove events widget
+    // to find widget ID, inspect the widget in browser
+    remove_meta_box("tribe_dashboard_widget", 'dashboard', 'normal');
+
+    // remove duplicator widget
+    remove_meta_box("duplicator_dashboard_widget", 'dashboard', 'normal');
+    
+}
+add_action('wp_dashboard_setup', 'remove_unwanted_dashboard_widgets');
+
+function custom_dashboard_help()
+{
+    $create_post_tutorial_url = get_stylesheet_directory_uri() . '/media/creating-a-post.mp4';
+
+    echo '<p>Watch the video below to learn how to create and edit a post:</p>';
+    echo '<video width="560" height="315" controls>
+            <source src="' . esc_url($create_post_tutorial_url) . '" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>';
+}
+add_action('wp_dashboard_setup', 'add_custom_dashboard_widget');
