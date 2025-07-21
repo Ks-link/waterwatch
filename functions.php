3cd65d6bc792bd53a64b227fcfa23af5944302e1
Kaleb Link <kaleb.s.link@gmail.com>
2025-07-21 16:44:35 -0700

    added another video tutorial to the dashboard

--- functions.php
+++ functions.php
@@ -87,6 +87,7 @@ function custom_dashboard_help()
 {
     $create_post_tutorial_url = get_stylesheet_directory_uri() . '/media/creating-a-post.mp4';
     $edit_page_tutorial_url = get_stylesheet_directory_uri() . '/media/edit-page.mp4';
+    $add_event_tutorial_url = get_stylesheet_directory_uri() . '/media/add-event.mp4';
 
     echo '<p>Watch the video below to learn how to create and edit a post:</p>';
     echo '<video width="560" height="315" controls>
@@ -97,6 +98,12 @@ function custom_dashboard_help()
     echo '<p>Watch the video below to learn how to edit a page:</p>';
     echo '<video width="560" height="315" controls>
             <source src="' . esc_url($edit_page_tutorial_url) . '" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>';
+
+    echo '<p>Watch the video below to learn how to add an event:</p>';
+    echo '<video width="560" height="315" controls>
+            <source src="' . esc_url($add_event_tutorial_url) . '" type="video/mp4">
             Your browser does not support the video tag.
         </video>';
 }
