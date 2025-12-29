429411775b79fe86579461d42ce6f05ac7bd4e56
Kaleb Link <kaleb.s.link@gmail.com>
2025-12-29 09:55:51 -0800

    added another tutorial video

--- functions.php
+++ functions.php
@@ -89,6 +89,7 @@ function custom_dashboard_help()
     $edit_page_tutorial_url = get_stylesheet_directory_uri() . '/media/edit-page.mp4';
     $add_event_tutorial_url = get_stylesheet_directory_uri() . '/media/add-event.mp4';
     $add_link_tutorial_url = get_stylesheet_directory_uri() . '/media/add-link.mp4';
+    $edit_text_tutorial_url = get_stylesheet_directory_uri() . '/media/edit-text.mp4';
 
     echo '<p>Watch the video below to learn how to create and edit a post:</p>';
     echo '<video width="560" height="315" controls>
@@ -111,6 +112,12 @@ function custom_dashboard_help()
     echo '<p>Watch the video below to learn how to add a link to a post:</p>';
     echo '<video width="560" height="315" controls>
             <source src="' . esc_url($add_link_tutorial_url) . '" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>';
+
+    echo '<p>Watch the video below to learn how to edit text (like bolding or italicizing):</p>';
+    echo '<video width="560" height="315" controls>
+            <source src="' . esc_url($edit_text_tutorial_url) . '" type="video/mp4">
             Your browser does not support the video tag.
         </video>';
 }
